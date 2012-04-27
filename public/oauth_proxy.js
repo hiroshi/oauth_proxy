d5db1cf4313378dddb112654d5b88e6f39749f7d
Hiroshi Saito <hiroshi3110@gmail.com>
2012-04-27 21:05:42 +0900

    Improve oauth_proxy.js for less verbose url specification on init and for login links.

--- public/oauth_proxy.js
+++ public/oauth_proxy.js
@@ -1,18 +1,26 @@
-window.OAuthProxy = {
-  init: function(url, site) {
-    return {
-      proxyUrl: url,
-      site: site,
-      get: function(url, callback) {
-        $.ajax({
-          url: this.proxyUrl + this.site + "/api",
-          data: {url: url},
-          dataType: "jsonp",
-          success: function(data) {
-            callback(data);
-          }
-        });
-      }
-    };
+window.OAuthProxy = function(site, options) {
+  var siteProxyUrl;
+  if (typeof(options) === "undefined") {
+    options = {};
   }
-}
+  if (!options.proxyUrl) {
+    options.proxyUrl = $("script[src$='oauth_proxy.js']").attr("src").replace("oauth_proxy.js","");
+  }
+  siteProxyUrl = options.proxyUrl + site;
+  if (options.loginSelector) {
+    $(options.loginSelector).attr({href: siteProxyUrl + "/login", target: "_blank"});
+  }
+  return {
+    siteProxyUrl: siteProxyUrl,
+    get: function(url, callback) {
+      $.ajax({
+        url: this.siteProxyUrl + "/api",
+        data: {url: url},
+        dataType: "jsonp",
+        success: function(data) {
+          callback(data);
+        }
+      });
+    }
+  };
+};
